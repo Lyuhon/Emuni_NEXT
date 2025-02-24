6a330d76fe6c6812d89b6dd852aa3e7d65b2a753
Lyuhon <aezakmi22990@gmail.com>
2025-02-24 14:48:40 +0500

    предрелизная сборка (ISR) v0.0.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
     images: {
         remotePatterns: [
             {
@@ -13,12 +12,4 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'export',
-// };
-
-// export default nextConfig;
 
